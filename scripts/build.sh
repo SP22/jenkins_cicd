d2d92e4f508a9e3ecce66b3f6976621526836b8c
ihor shyshov <sp.ukr.rs@gmail.com>
2025-01-23 00:19:17 +0200

    build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Update package list and install npm
-apt-get update
-apt-get install -y npm
-
-# Ensure npm is in the PATH
-export PATH=$PATH:/usr/local/bin
-
 # Install npm dependencies
 npm install
